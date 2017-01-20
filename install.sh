cfee4cf74abc1d3832d1c1042c064595a6d263aa
yenWu <c14006078@gmail.com>
2017-01-21 02:21:56 +0800

    New dependance pkg of compiling kernel and deb kernel

--- install.sh
+++ install.sh
@@ -12,3 +12,8 @@ sudo apt-get install -y\
 	libglib2.0-dev 	automake 	autoconf libtool libpixman-1-dev\
 	lib32gcc1 	lib32ncurses5	gcc-arm-none-eabi libnewlib-arm-none-eabi\
 	libtool		htop	minicom	wget	libusb-1.0-0-dev gdb gdb-arm-none-eabi
+
+# Denpendace Package of Compiling Linux Kernel
+## Reference: https://www.ubuntu-tw.org/modules/newbb/viewtopic.php?post_id=350806
+#sudo apt-get install -y\
+#	 fakeroot build-essential ncurses-dev xz-utils kernel-package\
